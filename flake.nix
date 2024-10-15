6525581db6a831b6c0cb952532ef4ef3bb0d6da5
Flokkq <webcla21@htl-kaindorf.at>
2024-10-16 00:05:33 +0200

    chore(repo): add flake and gitignore

--- flake.nix
+++ flake.nix
@@ -1,25 +1,34 @@
 {
-  description = "A basic flake for git-cliff";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem(system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [
+            (final: prev: {
+              # Overlay to explicitly choose Python 3.9
+              python = prev.python39;
+            })
+          ];
         };
       in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-                pkgs.git-cliff
-                pkgs.gnupg
-            ];
-          };
-        }
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.gnupg
+            pkgs.git-cliff
+
+            pkgs.python3 # Ensures Python 3.9 is installed
+            pkgs.python39Packages.pip # Add pip
+            pkgs.pyright 
+            pkgs.litecli
+          ];
+        };
+      }
     );
 }
